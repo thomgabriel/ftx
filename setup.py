70cf482b474e7703d77255d46bf9b4bd94ad841a
Gabriel <gabriel.thom02@gmail.com>
2020-08-19 20:32:08 -0300

    kaue cuck

--- setup.py
+++ setup.py
@@ -7,20 +7,20 @@ here = dirname(__file__)
 
 setup(name='ftx',
       version='0.0.1',
-      description="Unofficial python3 FTX exchange API.",
+      description="Unofficial python3 FTX exchange API 1.0.",
       long_description=open(join(here, 'README.md')).read(),
       license='MIT',
       author='thomgabriel',
       author_email='thomgabriel@protonmail.com',
-      url='https://github.com/quan-digital/nector/',
-      download_url = 'https://github.com/quan-digital/nector/dist/nector-0.0.1.tar.gz',
+      url='https://github.com/thomgabriel/ftx',
+      download_url = 'https://github.com/thomgabriel/ftx/dist/ftx-0.0.1.tar.gz',
       install_requires=[
         'requests==2.23.0',
         'schedule==0.6.0',
         'satoshi==0.1.3'
       ],
       packages=find_packages(),
-      keywords = ['nector', 'bitcoin', 'crypto-api', 'api-connector', 'exchange-api',
+      keywords = ['ftx', 'bitcoin', 'crypto-api', 'api-connector', 'exchange-api',
       'crypto-exchange', 'digital-currency', 'trading'],
       classifiers=[
         'Development Status :: 5 - Production/Stable',
@@ -36,7 +36,6 @@ setup(name='ftx',
         'Operating System :: OS Independent',
       ],
       )
-
 
 # PyPi publish flow
 # python3 setup.py sdist bdist_wheel
