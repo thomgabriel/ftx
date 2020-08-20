e8b16082cc6903f96882ec627892c9dd651093da
Gabriel <gabriel.thom02@gmail.com>
2020-08-20 15:47:22 -0300

    Update README and functions

--- ftx/api.py
+++ ftx/api.py
@@ -8,10 +8,10 @@ from ciso8601 import parse_datetime
 
 
 class FtxClient:
-    _ENDPOINT = 'https://ftx.com/api/'
 
-    def __init__(self, api_key=None, api_secret=None, subaccount_name=None) -> None:
+    def __init__(self, base_url=None, api_key=None, api_secret=None, subaccount_name=None) -> None:
         self._session = Session()
+        self._base_url = 'https://ftx.com/api/'
         self._api_key = api_key
         self._api_secret = api_secret
         self._subaccount_name = subaccount_name
@@ -26,9 +26,11 @@ class FtxClient:
         return self._request('DELETE', path, json=params)
 
     def _request(self, method: str, path: str, **kwargs) -> Any:
-        request = Request(method, self._ENDPOINT + path, **kwargs)
-        self._sign_request(request)
+        request = Request(method, self._base_url + path, **kwargs)
+        if self._api_key:
+            self._sign_request(request)
         response = self._session.send(request.prepare())
+        
         return self._process_response(response)
 
     def _sign_request(self, request: Request) -> None:
@@ -55,30 +57,37 @@ class FtxClient:
                 raise Exception(data['error'])
             return data['result']
 
-    def list_futures(self) -> List[dict]:
-        return self._get('futures')
+    
+    #
+    # Authentication required methods
+    #
 
-    def list_markets(self) -> List[dict]:
-        return self._get('markets')
+    def authentication_required(fn):
+        """Annotation for methods that require auth."""
+        def wrapped(self, *args, **kwargs):
+            if not self._api_key:
+                raise TypeError("You must be authenticated to use this method")
+            else:
+                return fn(self, *args, **kwargs)
+        return wrapped
 
-    def get_orderbook(self, market: str, depth: int = None) -> dict:
-        return self._get(f'markets/{market}/orderbook', {'depth': depth})
-
-    def get_trades(self, market: str) -> dict:
-        return self._get(f'markets/{market}/trades')
-
+    @authentication_required
     def get_account_info(self) -> dict:
         return self._get(f'account')
 
+    @authentication_required
     def get_open_orders(self, market: str = None) -> List[dict]:
         return self._get(f'orders', {'market': market})
     
+    @authentication_required
     def get_order_history(self, market: str = None, side: str = None, order_type: str = None, start_time: float = None, end_time: float = None) -> List[dict]:
         return self._get(f'orders/history', {'market': market, 'side': side, 'orderType': order_type, 'start_time': start_time, 'end_time': end_time})
-        
+   
+    @authentication_required
     def get_conditional_order_history(self, market: str = None, side: str = None, type: str = None, order_type: str = None, start_time: float = None, end_time: float = None) -> List[dict]:
         return self._get(f'conditional_orders/history', {'market': market, 'side': side, 'type': type, 'orderType': order_type, 'start_time': start_time, 'end_time': end_time})
 
+    @authentication_required
     def modify_order(
         self, existing_order_id: Optional[str] = None,
         existing_client_order_id: Optional[str] = None, price: Optional[float] = None,
@@ -95,13 +104,15 @@ class FtxClient:
             ** ({'clientId': client_order_id} if client_order_id is not None else {}),
         })
 
+    @authentication_required
     def get_conditional_orders(self, market: str = None) -> List[dict]:
         return self._get(f'conditional_orders', {'market': market})
 
+    @authentication_required
     def place_order(self, market: str, side: str, price: float, size: float, type: str = 'limit',
                     reduce_only: bool = False, ioc: bool = False, post_only: bool = False,
                     client_id: str = None) -> dict:
-        return self._post('orders', {'market': market,
+        return self._post(f'orders', {'market': market,
                                      'side': side,
                                      'price': price,
                                      'size': size,
@@ -111,7 +122,7 @@ class FtxClient:
                                      'postOnly': post_only,
                                      'clientId': client_id,
                                      })
-
+    @authentication_required
     def place_conditional_order(
         self, market: str, side: str, size: float, type: str = 'stop',
         limit_price: float = None, reduce_only: bool = False, cancel: bool = True,
@@ -129,14 +140,16 @@ class FtxClient:
         assert type not in ('trailing_stop',) or (trigger_price is None and trail_value is not None), \
             'Trailing stops need a trail value and cannot take a trigger price'
 
-        return self._post('conditional_orders',
+        return self._post(f'conditional_orders',
                           {'market': market, 'side': side, 'triggerPrice': trigger_price,
                            'size': size, 'reduceOnly': reduce_only, 'type': 'stop',
                            'cancelLimitOnTrigger': cancel, 'orderPrice': limit_price})
 
+    @authentication_required
     def cancel_order(self, order_id: str) -> dict:
         return self._delete(f'orders/{order_id}')
 
+    @authentication_required
     def cancel_orders(self, market_name: str = None, conditional_orders: bool = False,
                       limit_orders: bool = False) -> dict:
         return self._delete(f'orders', {'market': market_name,
@@ -144,20 +157,71 @@ class FtxClient:
                                         'limitOrdersOnly': limit_orders,
                                         })
 
+    @authentication_required
     def get_fills(self) -> List[dict]:
         return self._get(f'fills')
 
+    @authentication_required
     def get_balances(self) -> List[dict]:
-        return self._get('wallet/balances')
+        return self._get(f'wallet/balances')
 
+    @authentication_required
     def get_deposit_address(self, ticker: str) -> dict:
         return self._get(f'wallet/deposit_address/{ticker}')
 
+    @authentication_required
     def get_positions(self, show_avg_price: bool = False) -> List[dict]:
-        return self._get('positions', {'showAvgPrice': show_avg_price})
+        return self._get(f'positions', {'showAvgPrice': show_avg_price})
 
+    @authentication_required
     def get_position(self, name: str, show_avg_price: bool = False) -> dict:
         return next(filter(lambda x: x['future'] == name, self.get_positions(show_avg_price)), None)
+
+    @authentication_required
+    def set_leverage(self, leverage):
+        return self._post(f'account/leverage', {'leverage': leverage})
+
+    @authentication_required
+    def get_subaccounts(self) -> List[dict]:
+        return self._get(f'subaccounts')
+
+    @authentication_required
+    def create_subaccounts(self, nickname) -> List[dict]:
+        return self._post(f'subaccounts', {'nickname': nickname})
+
+    @authentication_required
+    def delete_subaccounts(self, nickname) -> List[dict]:
+        return self._delete(f'subaccounts', {'nickname': nickname})
+
+    @authentication_required
+    def get_subaccounts_balance(self, nickname) -> List[dict]:
+        return self._get(f'subaccounts/{nickname}/balances', {'nickname': nickname})
+
+    @authentication_required
+    def request_quote(self, fromCoin, toCoin , size) -> List[dict]:
+        return self._post(f'otc/quotes', {'fromCoin': fromCoin, 'toCoin': toCoin, 'size': size})
+
+    #
+    # Public methods
+    #
+
+    def get_futures(self) -> List[dict]:
+        return self._get(f'futures')
+
+    def get_future(self, future_name: str) -> dict:
+        return self._get(f'futures/{future_name}')
+
+    def get_markets(self) -> List[dict]:
+        return self._get(f'markets')
+
+    def get_market(self, market: str) -> dict:
+        return self._get(f'markets/{market}')
+
+    def get_orderbook(self, market: str, depth: int = None) -> dict:
+        return self._get(f'markets/{market}/orderbook', {'depth': depth})
+
+    def get_trades(self, market: str, limit: int = 100, start_time: float = None, end_time: float = None) -> dict:
+        return self._get(f'markets/{market}/trades', {'limit':limit, 'start_time': start_time, 'end_time': end_time})
 
     def get_all_trades(self, market: str, start_time: float = None, end_time: float = None) -> List:
         ids = set()
@@ -179,7 +243,12 @@ class FtxClient:
                 break
         return results
 
-    def get_historical_data(self,market_name,resolution,limit,start_time,end_time):
+    def get_historical_data(self,market_name: str,resolution: int ,limit: int ,start_time: float ,end_time: float ) -> dict:
         return self._get(f'markets/{market_name}/candles', dict(resolution=resolution,limit=limit,start_time=start_time,end_time=end_time))
 
-# GET /markets/{market_name}/candles?resolution={resolution}&limit={limit}&start_time={start_time}&end_time={end_time}
+    def get_future_stats(self, future_name) -> List[dict]:
+        return self._get(f'futures/{future_name}/stats', {'future_name' : future_name})
+
+    def get_funding_rates(self) -> List[dict]:
+        return self._get(f'funding_rates')
+    
